3e1a100784eb81f18fea2d34490a11cef9d5a3f2
MohamadGhanemDev <mohamad.d.ghanem@gmail.com>
2026-09-22 17:22:52 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+
+console.log(10 == "10");// true because this compares values only no matter what is the type string or num
+console.log(-100 == "-100");
+console.log(100 == "-100");
+
+
+console.log(10 != "10");// false
+
+// false === the name of this operator is Identical , it compares according to the value and type
+console.log(10 === "10");// 10 == "10"  => true , 10 === "10" true 10 == 10 but false because the second 10 is string
+
+//            not Identical
+console.log(10 !== "10");// true
+
+console.log(10 !== 10); // first value and second value is 10 they are equal and they have the same type Number so it return false
+
+console.log(10 > 20);
+console.log(10 > 10);
+console.log(10 >= 10);// two conditions 
+
+console.log(10 < 20);
+console.log(10 < 10);
+console.log(10 <= 10);
+
+
+console.log("Hamudy" === "Mohammad"); // false same type different value
+console.log(typeof "Hamudy" === typeof "Mohammad"); // true
+
+
+
